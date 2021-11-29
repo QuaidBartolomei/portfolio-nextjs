62afd1e838a5770c96dab704a1aa1cfe0459a8bd
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-11-29 13:44:19 -0800

    fix: page name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react'
 import Head from 'next/head'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material/styles'
@@ -19,7 +18,7 @@ export default function MyApp(props: MyAppProps) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>My page</title>
+        <title>Quaid</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
