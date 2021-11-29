d3c22ae3ede6386211db6af2932a3cada5c3878d
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-11-29 13:37:33 -0800

    fix: eslint settings

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,8 +2,8 @@ module.exports = {
   // format all file types recognized by prettier
   '*': ['prettier --ignore-unknown --write'],
 
-  // lint javascript after formatting
-  '*.{js,jsx}': ['eslint --fix --cache --quiet'],
+  // lint typescript after formatting
+  '*.{ts,tsx}': ['eslint --fix --cache --quiet'],
 
   // lint entire project if eslint settings changed
   '.eslint*': ['eslint . --cache'],
